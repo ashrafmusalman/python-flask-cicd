18b9398300ff31e549f821d6fc54c5250ccf5b77
Ashraf ali musalman <ashraf.musalman.ali@gmail.com>
2026-01-29 16:31:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask! again return hellow"
+    return "Hello, Flask! again return hellowwwwwwww"
 
 if __name__ == "__main__":
     app.run(debug=True)
